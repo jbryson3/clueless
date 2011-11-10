6d237569e1e801a4ab33000a6c7572786ec1d3b5
Al Anderson <sysala@Reddog-2.local>
2011-11-10 10:58:20 -0700

    added writeLog methods to Set_Cookie and Delete_Cookie

--- clueless_skeleton/js/game.js
+++ clueless_skeleton/js/game.js
@@ -48,29 +48,30 @@ var chosePiece = function(piece){
   return false;
 }
 
-function Set_Cookie( name, value, expires, path, domain, secure )
-{
-// set time, it's in milliseconds
-var today = new Date();
-today.setTime( today.getTime() );
+function Set_Cookie( name, value, expires, path, domain, secure ){
+   writeLog("Cookie Set!");
 
-/*
-if the expires variable is set, make the correct
-expires time, the current script below will set
-it for x number of days, to make it for hours,
-delete * 24, for minutes, delete * 60 * 24
-*/
-if ( expires )
-{
-expires = expires * 1000 * 60 * 60 * 24;
-}
-var expires_date = new Date( today.getTime() + (expires) );
+  // set time, it's in milliseconds
+  var today = new Date();
+  today.setTime( today.getTime() );
 
-document.cookie = name + "=" +escape( value ) +
-( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
-( ( path ) ? ";path=" + path : "" ) +
-( ( domain ) ? ";domain=" + domain : "" ) +
-( ( secure ) ? ";secure" : "" );
+  /*
+  if the expires variable is set, make the correct
+  expires time, the current script below will set
+  it for x number of days, to make it for hours,
+  delete * 24, for minutes, delete * 60 * 24
+  */
+  if ( expires )
+  {
+  expires = expires * 1000 * 60 * 60 * 24;
+  }
+  var expires_date = new Date( today.getTime() + (expires) );
+
+  document.cookie = name + "=" +escape( value ) +
+  ( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) +
+  ( ( path ) ? ";path=" + path : "" ) +
+  ( ( domain ) ? ";domain=" + domain : "" ) +
+  ( ( secure ) ? ";secure" : "" );
 }
 
 // this fixes an issue with the old method, ambiguous values
@@ -117,9 +118,10 @@ function Get_Cookie( check_name ) {
 
 // this deletes the cookie when called
 function Delete_Cookie( name, path, domain ) {
-if ( Get_Cookie( name ) ) document.cookie = name + "=" +
-( ( path ) ? ";path=" + path : "") +
-( ( domain ) ? ";domain=" + domain : "" ) +
-";expires=Thu, 01-Jan-1970 00:00:01 GMT";
+  writeLog("Cookie Deleted!");
+  if ( Get_Cookie( name ) ) document.cookie = name + "=" +
+    ( ( path ) ? ";path=" + path : "") +
+    ( ( domain ) ? ";domain=" + domain : "" ) +
+    ";expires=Thu, 01-Jan-1970 00:00:01 GMT";
 }
   
